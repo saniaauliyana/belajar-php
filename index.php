2f65942f217f04ae958232d09a22b905769b06b2
Sania Auliyana Diatri <saniaauli16@gmail.com>
2022-10-24 17:52:43 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello World!<br>";
+$nama = "Sania";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berumur $umur tahun. <br>";
+
+$namaAdik = "Jidan";
+$umurAdik =  17 ;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun <br>";
+
+$selisih = $umur-$umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisih <br>"
+?>
